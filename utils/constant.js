4564c12cedf6c01b8e468e575a79db9f12cddefb
naruls <76217299+naruls@users.noreply.github.com>
2023-09-02 14:46:15 +0300

    Delete utils directory

--- utils/constant.js
+++
@@ -1,9 +0,0 @@
-export const oneHourInMinutes = 60;
-export const shortFilmsDuration = 40;
-
-export const highResolutionCardOnScreenCount = 12;
-export const highResolutionCardAddCount = 4;
-export const mediumResolutionCardOnScreenCount = 8;
-export const mediumResolutionCardAddCount = 2;
-export const lowResolutionCardOnScreenCount = 5;
-export const lowResolutionCardAddCount = 2;
